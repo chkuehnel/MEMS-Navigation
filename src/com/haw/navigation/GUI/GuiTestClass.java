5c7101c640d21129a7dfba735bcd25c1e46578a2
chkuehnel <ch.kuehnel@gmx.de>
2014-10-18 12:32:50 +0200

    Unneeded class deleted

--- src/com/haw/navigation/GUI/GuiTestClass.java
+++
@@ -1,8 +0,0 @@
-package com.haw.navigation.GUI;
-
-/**
- * Created by chkue_000 on 24.09.2014.
- */
-public class GuiTestClass {
-    // This is only a placeholder. Please delete this class and add a new one!
-}
--- src/com/haw/navigation/Navigation/NavigationTestClass.java
+++
@@ -1,8 +0,0 @@
-package com.haw.navigation.Navigation;
-
-/**
- * Created by chkue_000 on 24.09.2014.
- */
-public class NavigationTestClass {
-    // This is only a placeholder. Please delete this class and add a new one!
-}
