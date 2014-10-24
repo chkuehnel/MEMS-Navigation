6b65c18185df19120cd77d9516db85cdd5e98e39
chkuehnel <ch.kuehnel@gmx.de>
2014-10-24 10:17:22 +0200

    Angle data for fixed angles added.

---
+++ src/com/haw/navigation/Navigation/FixedAngle.java
@@ -0,0 +1,40 @@
+package com.haw.navigation.Navigation;
+
+/**
+ * Created by chkue_000 on 24.10.2014.
+ */
+public class FixedAngle {
+    double phi;
+    double theta;
+    double psi;
+
+    public FixedAngle(double phi, double theta, double psi) {
+        this.phi = phi;
+        this.theta = theta;
+        this.psi = psi;
+    }
+
+    public double getPhi() {
+        return phi;
+    }
+
+    public void setPhi(double phi) {
+        this.phi = phi;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    public double getPsi() {
+        return psi;
+    }
+
+    public void setPsi(double psi) {
+        this.psi = psi;
+    }
+}
