8f1d324baaebaab34a7e50fba45d33a4666bd0c2
chkuehnel <ch.kuehnel@gmx.de>
2014-10-31 20:51:33 +0100

    Unneeded code deleted.

--- src/com/haw/navigation/Navigation/QuaternionClass.java
+++ src/com/haw/navigation/Navigation/QuaternionClass.java
@@ -49,11 +49,7 @@ public class QuaternionClass {
         double phi = - Math.atan((2*(q2*q3 + q0*q1))/(-q0*q0 + q1*q1 + q2*q2 - q3*q3))*180/Math.PI;
         double theta = Math.asin(2*(q0*q2 - q1*q3))*180/Math.PI;
         double psi = Math.atan((2*(q1*q2 + q0*q3))/(q0*q0 + q1*q1 - q2*q2 - q3*q3))*180/Math.PI;
-        /*
-        double phi = (Math.atan2((2*(q0*q1 + q2*q3)), (1 - 2*(q1*q1 + q2*q2) ))) *180/Math.PI;
-        double theta = (Math.atan(2*(q0*q2 - q3*q1)) *180/Math.PI);
-        double psi = Math.atan2((2*(q0*q3 + q1*q2)),(1 - 2*(q2*q2 + q3*q3))) *180/Math.PI;
-        */
+
         angleData = new FixedAngle(phi, theta, psi);
     }
 
