9bc28afd3490dcd3ef2c8d0ddb3c831dd91fe0c7
chkuehnel <ch.kuehnel@gmx.de>
2014-11-14 08:49:25 +0100

    Implement computing of acceleration data.

--- src/com/haw/navigation/Organiser.java
+++ src/com/haw/navigation/Organiser.java
@@ -4,22 +4,25 @@ import com.haw.navigation.Communication.SensorDataManager;
 import com.haw.navigation.GUI.MEMS_GUI;
 import com.haw.navigation.Navigation.QuaternionClass;
 import com.haw.navigation.Navigation.SensorDataSet;
+import com.haw.navigation.Navigation.SpeedWayClass;
 
 /**
  * Created by chkue_000 on 18.10.2014.
  */
 public class Organiser implements SensorDataManager.DataAvailableListener,
-        QuaternionClass.ResultAvailableListener{
+        SpeedWayClass.ResultAvailableListener{
 
     private static MEMS_GUI gui;
     private static QuaternionClass quaternionComputer;
+    private static SpeedWayClass wayComputer;
     private static SensorDataManager dataManager;
     private SensorDataSet dataSet;
 
     public Organiser() {
         dataManager = new SensorDataManager(this);
         dataManager.setListener(this);
-        quaternionComputer = new QuaternionClass(this);
+        quaternionComputer = new QuaternionClass();
+        wayComputer = new SpeedWayClass(this);
         gui = new MEMS_GUI(dataManager);
     }
 
@@ -30,6 +33,7 @@ public class Organiser implements SensorDataManager.DataAvailableListener,
             dataSet = dataManager.getDataSet();
             if (dataSet != null) {
                 quaternionComputer.fillDCM(dataSet.getGyroData());
+                wayComputer.computeWay(dataSet.getAccData(), quaternionComputer.getAngleData());
             }
         }
     }
@@ -38,7 +42,7 @@ public class Organiser implements SensorDataManager.DataAvailableListener,
     public void resultAvailable() {
         System.out.println("resultAvailable called.");
         if (dataSet != null) {
-            gui.updateLabel(quaternionComputer.getAngleData(), quaternionComputer.getQuaternion());
+            gui.updateLabel(quaternionComputer.getAngleData(), quaternionComputer.getQuaternion(), wayComputer.getNewSpeedWay());
         }
     }
 }
