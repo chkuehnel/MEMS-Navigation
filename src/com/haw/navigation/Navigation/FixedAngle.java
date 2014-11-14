755e9e48ac3f8aae7aabdbccd9fd14c420edc374
chkuehnel <ch.kuehnel@gmx.de>
2014-11-14 08:45:45 +0100

    Integrate norm data.

--- src/com/haw/navigation/Navigation/FixedAngle.java
+++ src/com/haw/navigation/Navigation/FixedAngle.java
@@ -8,10 +8,18 @@ public class FixedAngle {
     private double theta;
     private double psi;
 
+    private double normPhi;
+    private double normTheta;
+    private double normPsi;
+
     public FixedAngle(double phi, double theta, double psi) {
         this.phi = phi;
         this.theta = theta;
         this.psi = psi;
+
+        this.normPhi = phi / 180;
+        this.normTheta = theta / 180;
+        this.normPsi = psi / 180;
     }
 
     public double getPhi() {
@@ -36,5 +44,17 @@ public class FixedAngle {
 
     public void setPsi(double psi) {
         this.psi = psi;
+    }
+
+    public double getNormPhi() {
+        return normPhi;
+    }
+
+    public double getNormTheta() {
+        return normTheta;
+    }
+
+    public double getNormPsi() {
+        return normPsi;
     }
 }
